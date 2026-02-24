e1fdfec2d1290312b6022565c4416f7de78d4234
kafui-dev <kafui.the.dev@gmail.com>
2026-02-24 18:10:24 +0100

    Initial commit

---
+++ src/main/java/com/kafuidev/scinexus/ScinexusApplication.java
@@ -0,0 +1,13 @@
+package com.kafuidev.scinexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScinexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScinexusApplication.class, args);
+	}
+
+}
